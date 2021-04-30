bad819d9f52b2c2a6722960e22dd18b92d1662c6
SBRSV <12alexman@gmail.com>
2021-04-30 23:37:41 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={'color-picker'}>
+            <p className={'color-showcase'}>#ff33cc</p>
+            <button aria-label={'Generate Color'} className="color-box color-block center-by-flex dropdown-triangle">
+                <span className={'hex-bg'} style={{backgroundColor: '#ff33cc'}}/>
+            </button>
+            <div role={'list'} style={{display: 'none'}} className={'dropdown dropdown-large'}>
+                <div className={'range-slider '}>
+                    <span className={'range-slider-text'}>R</span>
+                    <input type="range" className={'range-slider-red'}/>
+                </div>
+                <div className={'range-slider'}>
+                    <span className={'range-slider-text'}>G</span>
+                    <input type="range" className={'range-slider-green'}/>
+                </div>
+                <div className={'range-slider'}>
+                    <span className={'range-slider-text'}>B</span>
+                    <input type="range" className={'range-slider-blue'}/>
+                </div>
+                <div className={'dropdown-buttons'}>
+                    <button className={'button button-second dropdown-buttons-first'}>cancel</button>
+                    <button className={'button button-primary'}>ok</button>
+                </div>
+            </div>
+
+            <button aria-label={'Choose Color'} className={'color-history color-block center-by-flex dropdown-triangle'}>
+                <span className={'arrow-down'}/>
+            </button>
+            {/* Add aria labeledby attr */}
+            <ul className={'dropdown dropdown-list'}>
+                <li className={'list-item'}>
+                    <button className={'color-button'} aria-label={'Color Red'}>
+                        <span className={'color-text'}>Red</span>
+                        <span className={'color-bg bg-red'}/>
+                    </button>
+                </li>
+                <li className={'list-item'}>
+                    <button className={'color-button'} aria-label={'Color Yellow'}>
+                        <span className={'color-text'}>Yellow</span>
+                        <span className={'color-bg bg-yellow'}/>
+                    </button>
+                </li>
+                <li className={'list-item'}>
+                    <button className={'color-button'} aria-label={'Color Green'}>
+                        <span className={'color-text'}>Green</span>
+                        <span className={'color-bg bg-green'}/>
+                    </button>
+                </li>
+                <li className={'list-item'}>
+                    <button className={'color-button'} aria-label={'Color Blue'}>
+                        <span className={'color-text'}>Blue</span>
+                        <span className={'color-bg bg-blue'}/>
+                    </button>
+                </li>
+            </ul>
+        </div>
+    )
 }
 
-export default App;
+export default App
