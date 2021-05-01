144f7b37f0abd6c04235eef182185f39c5268c60
SBRSV <12alexman@gmail.com>
2021-05-01 13:24:09 +0300

    toggle dropdown logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,103 @@
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
+import RangeSlider from './components/RangeSlider'
 import './App.scss'
 
 function App() {
+    const [toggleDropdownRgb, setToggleDropdownRgb] = useState(false)
+    const [toggleDropdownColor, setToggleDropdownColor] = useState(false)
+
+    const dropdownRgbRef = useRef<HTMLDivElement>(null)
+    const dropdownColorRef = useRef<HTMLUListElement>(null)
+
+    useEffect(() => {
+        document.body.addEventListener('click', (e: MouseEvent) => {
+            if (dropdownRgbRef?.current?.contains(e.target as HTMLElement) || dropdownColorRef?.current?.contains(e.target as HTMLElement)) {
+                return
+            }
+
+            setToggleDropdownRgb(false)
+            setToggleDropdownColor(false)
+        })
+    }, [])
+
+    const handleDropdownToggle = (e: React.MouseEvent, whichToggle?: string) => {
+        e.stopPropagation()
+
+        if (whichToggle === 'dropdown-rgb') {
+            setToggleDropdownColor(false)
+            setToggleDropdownRgb(!toggleDropdownRgb)
+        } else {
+            setToggleDropdownRgb(false)
+            setToggleDropdownColor(!toggleDropdownColor)
+        }
+    }
+
     return (
         <div className={'color-picker'}>
             <p className={'color-showcase'}>#ff33cc</p>
-            <button aria-label={'Generate Color'} className="color-box color-block center-by-flex dropdown-triangle">
+            <button
+                aria-label={'Generate Color'}
+                className="color-box color-block center-by-flex"
+                onClick={(e) => handleDropdownToggle(e, 'dropdown-rgb')}
+            >
                 <span className={'hex-bg'} style={{backgroundColor: '#ff33cc'}}/>
             </button>
-            <div role={'list'} style={{display: 'none'}} className={'dropdown dropdown-large'}>
-                <div className={'range-slider '}>
-                    <span className={'range-slider-text'}>R</span>
-                    <input type="range" className={'range-slider-red'}/>
+            {toggleDropdownRgb &&
+                <div ref={dropdownRgbRef} role={'list'} className={'dropdown dropdown-large dropdown-rgb'} id={'dropdown-rgb'}>
+                    <RangeSlider
+                        className={'range-slider-red'}
+                        colorText={'R'}
+                    />
+                    <RangeSlider
+                        className={'range-slider-green'}
+                        colorText={'G'}
+                    />
+                    <RangeSlider
+                        className={'range-slider-blue'}
+                        colorText={'B'}
+                    />
+                    <div className={'dropdown-buttons'}>
+                        <button className={'button button-second dropdown-buttons-first'}>cancel</button>
+                        <button className={'button button-primary'}>ok</button>
+                    </div>
                 </div>
-                <div className={'range-slider'}>
-                    <span className={'range-slider-text'}>G</span>
-                    <input type="range" className={'range-slider-green'}/>
-                </div>
-                <div className={'range-slider'}>
-                    <span className={'range-slider-text'}>B</span>
-                    <input type="range" className={'range-slider-blue'}/>
-                </div>
-                <div className={'dropdown-buttons'}>
-                    <button className={'button button-second dropdown-buttons-first'}>cancel</button>
-                    <button className={'button button-primary'}>ok</button>
-                </div>
-            </div>
+            }
 
-            <button aria-label={'Choose Color'} className={'color-history color-block center-by-flex dropdown-triangle'}>
-                <span className={'arrow-down'}/>
+            <button
+                aria-label={'Choose Color'}
+                className={'color-history color-block center-by-flex'}
+                onClick={(e) => handleDropdownToggle(e)}
+            >
+                <span className={'arrow-down'} />
             </button>
-            {/* Add aria labeledby attr */}
-            <ul className={'dropdown dropdown-list'}>
-                <li className={'list-item'}>
-                    <button className={'color-button'} aria-label={'Color Red'}>
-                        <span className={'color-text'}>Red</span>
-                        <span className={'color-bg bg-red'}/>
-                    </button>
-                </li>
-                <li className={'list-item'}>
-                    <button className={'color-button'} aria-label={'Color Yellow'}>
-                        <span className={'color-text'}>Yellow</span>
-                        <span className={'color-bg bg-yellow'}/>
-                    </button>
-                </li>
-                <li className={'list-item'}>
-                    <button className={'color-button'} aria-label={'Color Green'}>
-                        <span className={'color-text'}>Green</span>
-                        <span className={'color-bg bg-green'}/>
-                    </button>
-                </li>
-                <li className={'list-item'}>
-                    <button className={'color-button'} aria-label={'Color Blue'}>
-                        <span className={'color-text'}>Blue</span>
-                        <span className={'color-bg bg-blue'}/>
-                    </button>
-                </li>
-            </ul>
+            {toggleDropdownColor &&
+                <ul ref={dropdownColorRef} className={'dropdown dropdown-list'}>
+                    <li className={'list-item'}>
+                        <button className={'color-button'} aria-label={'Color Red'}>
+                            <span className={'color-text'}>Red</span>
+                            <span className={'color-bg bg-red'}/>
+                        </button>
+                    </li>
+                    <li className={'list-item'}>
+                        <button className={'color-button'} aria-label={'Color Yellow'}>
+                            <span className={'color-text'}>Yellow</span>
+                            <span className={'color-bg bg-yellow'}/>
+                        </button>
+                    </li>
+                    <li className={'list-item'}>
+                        <button className={'color-button'} aria-label={'Color Green'}>
+                            <span className={'color-text'}>Green</span>
+                            <span className={'color-bg bg-green'}/>
+                        </button>
+                    </li>
+                    <li className={'list-item'}>
+                        <button className={'color-button'} aria-label={'Color Blue'}>
+                            <span className={'color-text'}>Blue</span>
+                            <span className={'color-bg bg-blue'}/>
+                        </button>
+                    </li>
+                </ul>
+            }
         </div>
     )
 }
